e1ae6a9d917ec24406c4bc33e8542eade2f22c6d
Tayyyb <awantayyyab44@gmail.com>
2024-05-06 12:27:01 +0500

    fix the bugs

--- src/index.js
+++ src/index.js
@@ -1,29 +1,20 @@
-import dotenv  from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import {app} from './app.js'
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './.env'
-})
+  path: "./.env",
+});
 
-connectDB ()
-.then( () => {
+connectDB()
+  .then(() => {
     app.listen(process.env.PORT || 6000, () => {
-        console.log(`Server is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err)=>{
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
     console.log("MONGODB CONNECTION FAILED !!! ", err);
-})
-
-
-
-
-
-
-
-
-
+  });
 
 /*
 import express from "express"
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,23 +1,22 @@
 class ApiError extends Error {
-    constructor(
-satutsCode,
-message= "Something went wrong",
-errors = [],
-stack =""
-    ){
-        super(message)
-        this.satutsCode = satutsCode
-        this.data = null,
-        this.message = message
-        this.success = false;
-        this.errors = errors
+  constructor(
+    satutsCode,
+    message = "Something went wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.satutsCode = satutsCode;
+    (this.data = null), (this.message = message);
+    this.success = false;
+    this.errors = errors;
 
-        if (stack){
-            this.stack = stack
-        } else {
-            Error.captureStackTrace(this,this.constructor)
-        }
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
     }
+  }
 }
 
-export {ApiError}
+export { ApiError };
